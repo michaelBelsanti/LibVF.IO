280c3bdb77fe5f5fd8390804f63e365de20825a3
michaelBelsanti <quasigod-io@protonmail.com>
2023-03-17 15:05:33 -0400

    Updated flake to use nimble2nix

--- flake.nix
+++ flake.nix
@@ -2,40 +2,27 @@
   description = "libvfio flake";
 
   inputs = {
-    utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nvidia-vgpu.url = "github:danielfullmer/nixos-nvidia-vgpu";
+    nimble.url = "github:nix-community/flake-nimble";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nimble2nix.url = "github:bandithedoge/nimble2nix";
   };
 
-  outputs = inputs@{ self, nixpkgs, utils, ... }:
-    let
-      # put devShell and any other required packages into local overlay
-      localOverlay = import ./nix/overlay.nix;
-
-      # if you have additional overlays from other flakes, you may add them here
-      allOverlays = [
-        localOverlay # this should expose devShell
+  outputs = inputs@{flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
       ];
-
-      pkgsForSystem = system: import nixpkgs {
-        overlays = allOverlays;
-        inherit system;
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [inputs.nimble2nix.overlay];
+        };
+        packages.default = pkgs.buildNimblePackage {
+          pname = "arcd";
+          version = "0.1";
+          src = ./.;
+        };
       };
-    # https://github.com/numtide/flake-utils#usage for more examples
-    in utils.lib.eachSystem [ "x86_64-linux" ] (system: rec {
-      legacyPackages = pkgsForSystem system;
-      packages = utils.lib.flattenTree {
-        inherit (legacyPackages) devShell libvfio;
-      };
-      defaultPackage = packages.libvf.io;
-      apps.libvf.io = utils.lib.mkApp { drv = packages.libvf.io; };  # use as `nix run .#libvf.io`
-      hydraJobs = { inherit (legacyPackages) libvfio; };
-      checks = { inherit (legacyPackages) libvfio; };              # items to be ran as part of `nix flake check`
-  }) // {
-    # non-system suffixed items should go here
-    overlays = {
-      default = localOverlay;
     };
-    nixosModules.default = import ./nix/module.nix inputs; # attr set or list
-    nixosConfigurations.hostname = { config, pkgs, ... }: {};
-  };
 }
